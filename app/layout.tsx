3f0803976925063ea44a626f82cda56ef1448828
SM <52240990+baesumin@users.noreply.github.com>
2025-05-02 11:16:02 +0900

    chore: og태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,33 @@ import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Random Generator",
-  description: "Random Generator Landing Page",
+  description:
+    "Flip coins, roll dice, spin roulette wheels and more! The ultimate random generator app that makes decisions fun and easy.",
   generator: "alpaca developer",
+  metadataBase: new URL("https://random-generator-all-in-one.vercel.app/"),
+  openGraph: {
+    type: "website",
+    url: "https://random-generator-all-in-one.vercel.app/",
+    title: "Random Generator - All Your Random Needs in One App",
+    description:
+      "Flip coins, roll dice, spin roulette wheels and more! The ultimate random generator app that makes decisions fun and easy.",
+    siteName: "Random Generator",
+    images: [
+      {
+        url: "https://random-generator-all-in-one.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Random Generator App",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Random Generator - All Your Random Needs in One App",
+    description:
+      "Flip coins, roll dice, spin roulette wheels and more! The ultimate random generator app that makes decisions fun and easy.",
+    images: ["https://random-generator-all-in-one.vercel.app/og-image.png"],
+  },
 };
 
 export default function RootLayout({
